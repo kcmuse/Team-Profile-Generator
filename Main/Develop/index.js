4e2c8cb254b4a212dd931e7170ffd7b6750c3b68
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-07-19 14:59:22 -0700

    updated base file structure

---
+++ Main/Develop/index.js
@@ -0,0 +1,24 @@
+const Manager = require(`./lib/Manager`);
+const Intern = require(`./lib/Intern`);
+const Engineer = require(`./lib/Engineer`);
+const generate = require(`./util/generateHtml`);
+
+
+// TODO: WHEN I am prompted for my team members and their information
+//WHEN I start the application Questions
+// TODO: THEN I am prompted to enter the team manager’s // name, // employee ID, // email address, and // office number
+// TODO: THEN I am presented with a menu with the option to add an // engineer or an // intern or to // finish building my team
+// TODO: THEN I am prompted to enter the engineer’s // name, // ID, // email, and //GitHub username, and I am taken back to the menu
+// TODO: THEN I am prompted to enter the intern’s //name, //ID, //email, and //school, and I am taken back to the menu
+
+
+
+
+
+
+
+
+
+
+// WHEN I decide to finish building my team THEN I exit the application, and the HTML is generated
+// TODO: THEN an HTML file is generated that displays a nicely formatted team roster based on user input
