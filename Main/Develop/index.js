dcc3deec1b95a96cef25e6e0272999c3aa1fd1db
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-07-21 14:01:05 -0700

    creates html

--- Main/Develop/index.js
+++ Main/Develop/index.js
@@ -5,40 +5,33 @@ const generate = require(`./util/generateHtml`);
 const inquirer = require("inquirer");
 const fs = require(`fs`);
 
+
+let HTML = ``;
 const employeeList = [];
 
-// TODO: WHEN I am prompted for my team members and their information
-//WHEN I start the application Questions
-// TODO: THEN I am prompted to enter the team manager’s // name, // employee ID, // email address, and // office number
-// TODO: THEN I am presented with a menu with the option to add an // engineer or an // intern or to // finish building my team
-// TODO: THEN I am prompted to enter the engineer’s // name, // ID, // email, and //GitHub username, and I am taken back to the menu
-// TODO: THEN I am prompted to enter the intern’s //name, //ID, //email, and //school, and I am taken back to the menu
 function askQuestion() {
     inquirer.prompt([{
-        name: "question",
+        name: "role",
         type: "list",
         message: `What Employee are you making a profile for?`,
         choices: ["Team Manager", "Team Engineer", "Team Intern", "Finished"]
     }]).then(answers => {
-        switch (answers.question) {
+        switch (answers.role) {
             case "Team Manager":
-                // console.log("Add Manager!")
                 addManager();
                 break;
 
             case "Team Engineer":
-                // console.log("Add Engineer!")
                 addEngineer();
                 break;
 
             case "Team Intern":
-                // console.log("Add Intern!")
                 addIntern();
                 break;
 
             case "Finished":
-                fs.writeFile(`employees.html`, employeeList, err => console.log(err))
-                    // console.log(`Your Teams Profile has been created Thanks!`);
+                HTML += generate(employeeList);
+                fs.writeFile(`myTeam.html`, HTML, err => console.log(err))
                 break;
         }
     })
@@ -58,13 +51,12 @@ function addManager() {
         message: "Email address?",
         name: "email"
     }, {
-        type: "input",
+        type: "number",
         message: "Office number?",
         name: "officeNumber"
     }]).then(answer => {
         const employee = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
         employeeList.push(employee)
-        console.log(employeeList);
         askQuestion();
     })
 }
@@ -87,10 +79,8 @@ function addEngineer() {
         message: "Github Username?",
         name: "github"
     }]).then(answer => {
-        // console.log(answer.name, answer.id, answer.email, answer.github);
         const employee = new Engineer(answer.name, answer.id, answer.email, answer.github);
         employeeList.push(employee)
-        console.log(employeeList);
         askQuestion();
     })
 }
@@ -113,14 +103,10 @@ function addIntern() {
         message: "Interns School?",
         name: "school"
     }]).then(answer => {
-        // console.log(answer.name, answer.id, answer.email, answer.school);
         const employee = new Intern(answer.name, answer.id, answer.email, answer.school);
         employeeList.push(employee)
-        console.log(employeeList);
         askQuestion();
     })
 }
 
 askQuestion();
-// WHEN I decide to finish building my team THEN I exit the application, and the HTML is generated
-// TODO: THEN an HTML file is generated that displays a nicely formatted team roster based on user input
--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Team-Profile-Generator
+
+## Description
+
+This projec is designed create profiles for team members. Users are first asked who they are making a profile for, then based on input a series of questions is asked. Once all questions have been asked for that employee, they are then asked if they would like to make more profiles. If not users may select "Finsished" if that is selected the process is terminated and a new HTML page is generated with information for each team member created.
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Testing](#testing)
+* [Questions](#questions)
+
+## Installation
+
+Below is the instructions to install the dependencies:
+
+```
+npm i 
+will install all i needed files
+```
+
+## Usage
+![Preview]()
+npm start
+ will begin the prompt and you can start creating the file
+
+## Contribution
+
+All contributions can be made to my github
+
+## Testing
+
+For testing, run the following command:
+
+```
+npm test
+```
+
+
+## Questions
+
+For all questions please reach out to me at my email or github!
+* [GitHub](https://github.com/kcmuse)
+* [eMail](mailto:kcmusedev@gmail.com)
+
